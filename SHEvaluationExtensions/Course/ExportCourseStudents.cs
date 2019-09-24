2fb0ae937643f64c9ec9a3e8d8dcf042bba01596
chuntaichen <ct.chen@ischool.com.tw>
2019-09-24 11:47:48 +0800

    匯出加入新增欄位："及格標準", "補考標準", "科目代碼"，並資料讀取層使用SQL重構。

--- SHEvaluationExtensions/Course/ExportCourseStudents.cs
+++ SHEvaluationExtensions/Course/ExportCourseStudents.cs
@@ -4,6 +4,8 @@ using System.Text;
 using SmartSchool.API.PlugIn;
 using SHSchool.Data;
 using FISCA.LogAgent;
+using FISCA.Data;
+using System.Data;
 
 namespace SHEvaluationExtensions.Course
 {
@@ -21,77 +23,84 @@ namespace SHEvaluationExtensions.Course
 
         public override void InitializeExport(SmartSchool.API.PlugIn.Export.ExportWizard wizard)
         {
-            wizard.ExportableFields.AddRange("姓名", "學號", "班級", "座號","必選修","校部訂","學生狀態");
+            wizard.ExportableFields.AddRange("姓名", "學號", "班級", "座號","必選修","校部訂", "及格標準", "補考標準", "科目代碼", "學生狀態");
             wizard.ExportPackage += delegate(object sender, SmartSchool.API.PlugIn.Export.ExportPackageEventArgs e)
             {
-                //課程資訊
-                List<SHCourseRecord> courses = SHCourse.SelectByIDs(e.List);
-                //學生修課資訊
-                Dictionary<string, List<SHSCAttendRecord>> scattends = new Dictionary<string, List<SHSCAttendRecord>>();
-                //課程修課學生
-                Dictionary<string,SHStudentRecord> students = new Dictionary<string, SHStudentRecord>();
-
-                #region 取得修課記錄
-                foreach (SHSCAttendRecord record in SHSCAttend.SelectByStudentIDAndCourseID(new string[] { }, e.List))
-                {                    
-                    if (!scattends.ContainsKey(record.RefCourseID))
-                        scattends.Add(record.RefCourseID, new List<SHSCAttendRecord>());
-                    scattends[record.RefCourseID].Add(record);
-
-                    if (!students.ContainsKey(record.RefStudentID))
-                        students.Add(record.RefStudentID, null);
-                }
-                #endregion
-
-                #region 取得學生資訊
-                SHStudent.RemoveAll();
-                foreach (SHStudentRecord record in SHStudent.SelectByIDs(new List<string>(students.Keys)))
+                // 課程 ID
+                List<string> courseIDList = e.List;
+                
+                if (courseIDList.Count > 0)
                 {
-                    if (students.ContainsKey(record.ID))
-                        students[record.ID] = record;
-                }
-                #endregion
+                    // 取得學生修課資訊
+                    Dictionary<string, Dictionary<string, DataRow>> SCAttendDict = new Dictionary<string, Dictionary<string, DataRow>>();
 
-                #region 產生 Row Data
-                foreach (SHCourseRecord course in courses)
-                {
-                    //Debug
-                    if (!scattends.ContainsKey(course.ID)) continue;
+                    // 取得學生修課資料
+                    string qrySCAttend = "SELECT " +
+                    "student.name AS student_name" +
+                    ",student.student_number" +
+                    ",class.class_name" +
+                    ",student.seat_no" +
+                    ",CASE is_required WHEN '1' THEN '必修' WHEN '0' THEN '選修' ELSE '' END AS is_required" +
+                    ",CASE required_by WHEN 1 THEN '部訂' WHEN 2 THEN '校訂' ELSE '' END AS required_by" +
+                    ",ref_course_id AS course_id" +
+                    ",ref_student_id AS student_id" +
+                    ",passing_standard" +
+                    ",makeup_standard" +
+                    ",remark" +
+                    ",designate_final_score" +
+                    ",subject_code" +
+                    ",CASE student.status WHEN 1 THEN '一般' WHEN 2 THEN '延修' WHEN 4 THEN '休學' WHEN 8 THEN '輟學' WHEN 16 THEN '畢業或離校' WHEN 256 THEN '刪除' END AS status" +
+                    " FROM sc_attend INNER JOIN student" +
+                    " ON sc_attend.ref_student_id = student.id INNER JOIN class" +
+                    " ON student.ref_class_id = class.id  WHERE ref_course_id IN(" + string.Join(",", courseIDList.ToArray()) + ")" +
+                    " ORDER BY class_name,seat_no,student_number;";
 
-                    foreach (SHSCAttendRecord record in scattends[course.ID])
+                    QueryHelper qh = new QueryHelper();
+                    
+                    DataTable dtSCAttend = qh.Select(qrySCAttend);
+
+                    #region 產生 Row Data
+                    foreach (DataRow dr in dtSCAttend.Rows)
                     {
                         RowData row = new RowData();
-                        row.ID = course.ID;
+                        row.ID = GetFieldString(dr, "course_id");
                         foreach (string field in e.ExportFields)
                         {
                             if (wizard.ExportableFields.Contains(field))
                             {
                                 switch (field)
                                 {
-                                    case "姓名": row.Add(field, students[record.RefStudentID].Name); break;
-                                    case "學號": row.Add(field, students[record.RefStudentID].StudentNumber); break;
-                                    case "班級": row.Add(field, (students[record.RefStudentID].Class != null ? students[record.RefStudentID].Class.Name : "")); break;
-                                    case "座號": row.Add(field, "" + students[record.RefStudentID].SeatNo); break;
-                                    case "必選修":
-                                            if (record.Required)
-                                                row.Add(field, "" + "必修");
-                                            else
-                                                row.Add(field, "" + "選修");
-                                        break;
-                                    case "校部訂":                                        
-                                            row.Add(field, "" + record.RequiredBy);                                        
-                                        break;
-                                    case "學生狀態": row.Add(field, "" + students[record.RefStudentID].Status.ToString()); break;
+                                    case "姓名": row.Add(field, GetFieldString(dr, "student_name")); break;
+                                    case "學號": row.Add(field, GetFieldString(dr, "student_number")); break;
+                                    case "班級": row.Add(field, GetFieldString(dr, "class_name")); break;
+                                    case "座號":row.Add(field, GetFieldString(dr, "seat_no"));break;
+                                    case "必選修":row.Add(field, GetFieldString(dr, "is_required"));break;
+                                    case "校部訂":row.Add(field, GetFieldString(dr, "required_by"));break;
+                                    case "及格標準": row.Add(field, GetFieldString(dr, "passing_standard")); break;
+                                    case "補考標準": row.Add(field, GetFieldString(dr, "makeup_standard")); break;
+                                    case "科目代碼": row.Add(field, GetFieldString(dr, "subject_code")); break;
+                                    case "學生狀態": row.Add(field, GetFieldString(dr, "status")); break;
                                 }
                             }
                         }
                         e.Items.Add(row);
+
                     }
-                }
-                #endregion
-                                
-                ApplicationLog.Log("成績系統.匯入匯出", "匯出課程修課學生", "總共匯出" + e.Items.Count + "筆課程修課學生。");
+
+                    #endregion
+
+                    ApplicationLog.Log("成績系統.匯入匯出", "匯出課程修課學生", "總共匯出" + e.Items.Count + "筆課程修課學生。");
+                }                
             };
         }
+
+        private string GetFieldString(DataRow dr,string name)
+        {
+            string value = "";
+            if (dr[name] != null)
+                value = dr[name].ToString();
+            return value;
+        }
+        
     }
 }
