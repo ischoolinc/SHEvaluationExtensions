292cdf95a4f6f71a33fdd9ec25adc51950ac9411
CT Chen <ct.chen@ischool.com.tw>
2024-12-28 15:45:51 +0800

    調整功能名稱。

--- TechnologyStar2020/Program.cs
+++ TechnologyStar2020/Program.cs
@@ -14,13 +14,15 @@ namespace TechnologyStar2020
         [FISCA.MainMethod(FISCA.StartupPriority.LastAsynchronized)]
         public static void Main()
         {
+
+            // 過時不使用，先用這方式隱藏
             #region 技職繁星報名資料
             string guid = "99075CC9-91BD-48B7-94FE-C57CF299C93E";
 
             // 透過安裝這程式讓主要計算功能可以使用
-            MotherForm.RibbonBarItems["教務作業", "批次作業/檢視"]["成績排名"]["技職繁星成績排名計算"].Visible = true;
+            MotherForm.RibbonBarItems["教務作業", "批次作業/檢視"]["成績排名"]["技職繁星成績排名計算"].Visible = false;
             MotherForm.RibbonBarItems["教務作業", "批次作業/檢視"]["成績排名"]["技職繁星成績排名資料檢索"].Visible = true;
-
+                        
             RibbonBarItem rbItem1 = MotherForm.RibbonBarItems["教務作業", "資料統計"];
             rbItem1["報表"]["技職繁星報名資料"].Enable = UserAcl.Current[guid].Executable;
             rbItem1["報表"]["技職繁星報名資料"].Click += delegate
@@ -28,6 +30,7 @@ namespace TechnologyStar2020
                 UI.PrintForm pr = new UI.PrintForm();
                 pr.ShowDialog();
             };
+            rbItem1["報表"]["技職繁星報名資料"].Visible = false;
 
             // 技職繁星
             Catalog catalog1a = RoleAclSource.Instance["教務作業"]["功能按鈕"];
